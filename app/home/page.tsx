cde734e15d774e9768e38a6b33569a59df5c71e2
memymini <minhi0614@gmail.com>
2025-05-17 20:37:17 +0900

    feat: home page 레이아웃 구현

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,22 @@
-import Header from "../../components/Header";
+"use client";
+import styles from "./page.module.css";
 
 export default function HomePage() {
 
   return (
-    <div>
-      Home Page
+    <div className={styles.home_container}>
+      <div className={styles.search_box_container}>
+        <input
+          type="text"
+          className={styles.search_box}
+          placeholder="할 일을 입력해주세요"
+        />
+        <button className={styles.add_button}>추가하기</button>
+      </div>
+      <div className={styles.list_container}>
+        <div className={styles.todo_list_container}></div>
+        <div className={styles.done_list_container}></div>
+      </div>
     </div>
   );
 }
