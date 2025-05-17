5de37617851ac8f411e8945ea3279e5af63f45da
memymini <minhi0614@gmail.com>
2025-05-18 02:35:33 +0900

    fix: 홈페이지 반응형 뷰 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,30 +1,19 @@
-"use client";
-import useResponsive from "../hooks/useResponsive";
-import Image from "next/image";
 import styles from "./Header.module.css";
 
 export default function Header() {
-    const device = useResponsive();
-
-    const imageSrc =
-        device === "mobile"
-            ? "/banner-small.png"
-            : "/banner.png";
-
-    const imageSize =
-        device === "mobile"
-            ? { width: 71, height: 40 }
-            : { width: 151, height: 40 };
-
     return (
         <div className={styles.header_container}>
             <div className={styles.banner_container}>
-                <Image
-                    src={imageSrc}
-                    alt="banner"
-                    width={imageSize.width}
-                    height={imageSize.height}
-                />
+                <picture>
+                    <source media="(max-width: 768px)" srcSet="/banner-small.png" />
+                    <img
+                        src="/banner.png"
+                        alt="banner"
+                        width={151}
+                        height={40}
+                        className={styles.banner_image}
+                    />
+                </picture>
             </div>
         </div>
     );
